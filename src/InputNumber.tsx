4ae98802a9040f7c515afb597cb651bb09363593
Doma <leishenghao@126.com>
2022-04-13 17:45:40 +0800

    fix(InputNumber): work with increment/decrement buttons

--- src/InputNumber.tsx
+++ src/InputNumber.tsx
@@ -18,8 +18,17 @@ export function InputNumber({
   return (
     <RsInputNumber
       {...field}
+      // Don't use field.onChange here
+      // because event can be other type than change
+      // e.g. MouseEvent from stepUp/stepDown button
+      //      KeyboardEvent from ArrowUp/ArrowDown keys
       onChange={(newValue: any, event) => {
-        field.onChange(event);
+        const parsed = parseFloat(newValue);
+
+        // Handle target.value as text
+        // copied from field.onChange
+        // see https://github.com/jaredpalmer/formik/blob/e677bea8181f40e6762fc7e7fb009122384500c6/packages/formik/src/Formik.tsx#L630
+        form.setFieldValue(field.name, isNaN(parsed) ? newValue : parsed);
         onChange?.(newValue, event);
       }}
       {...props}
--- src/__tests__/InputNumber.test.tsx
+++ src/__tests__/InputNumber.test.tsx
@@ -32,3 +32,32 @@ test("适配 formik", async () => {
     expect(formikRef.current?.values).toHaveProperty("age", 29);
   });
 });
+
+test("点击增加/减少按钮有效", async () => {
+  const formikRef = React.createRef<FormikProps<{ age: number }>>();
+  const { container } = render(
+    <Formik
+      innerRef={formikRef}
+      initialValues={{
+        age: 18,
+      }}
+      onSubmit={_.noop}
+    >
+      <Form>
+        <Field name="age" component={InputNumber} />
+      </Form>
+    </Formik>
+  );
+
+  userEvent.click(container.querySelector(".rs-input-number-touchspin-up")!);
+
+  await waitFor(() => {
+    expect(formikRef.current?.values.age).toBe(19);
+  });
+
+  userEvent.click(container.querySelector(".rs-input-number-touchspin-down")!);
+
+  await waitFor(() => {
+    expect(formikRef.current?.values.age).toBe(18);
+  });
+});
